e5a547e0fead9dd7e5b774fc2e988a59126b81da
maheshnicham <mahesh.nicham@gmail.com>
2022-12-25 16:20:16 +0530

    End of section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -15,13 +17,17 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+               options.JsonSerializerOptions.Converters.Insert(0, new DateOnlyJsonConverter());
+            });
             services.AddApplicationServices(_config);
             services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             
             if (env.IsDevelopment())
@@ -44,6 +50,20 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+
+            using var scope = app.ApplicationServices.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedUsers(context);
+            }
+            catch (System.Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Startup>>();
+                logger.LogError(ex, "An error ocurred during migration.");
+            }
         }
     }
 }
